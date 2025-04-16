283386b933edfc0bcd38eb85c9d48e95384a8285
Zendii-cmd <giabao.tran020824@gmail.com>
2025-04-16 11:58:19 +0700

    Đã thêm Models (Danh mục và Sản Phẩm) V1

--- config/db.js
+++ config/db.js
@@ -20,12 +20,40 @@ async function initSampleData() {
     }
 
     const NguoiDung = require('../models/NguoiDung.model');
+    const DanhMuc = require('../models/DanhMuc.model');
+    const SanPham = require('../models/SanPham.model');
+
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash('123456', saltRounds);
 
     // Xóa dữ liệu cũ (cẩn thận!)
     await mongoose.connection.dropDatabase();
     console.log('Đã xóa toàn bộ dữ liệu cũ');
+
+     // Tạo danh mục
+     const danhMuc1 = await DanhMuc.create({
+      tenDanhMuc: 'Đồ ăn nhanh',
+      moTa: 'Các món ăn nhanh phổ biến'
+    });
+    
+    const danhMuc2 = await DanhMuc.create({
+      tenDanhMuc: 'Đồ uống',
+      moTa: 'Các loại đồ uống'
+    });
+    
+    // Tạo sản phẩm
+    await SanPham.create({
+      tenSanPham: 'Veggie tomato',
+      moTa: 'Bánh kẹp rau củ tươi ngon',
+      danhMuc: danhMuc1._id,
+      gia: 9.22,
+      calo: 150,
+      thoiGianChuanBi: 15,
+      kichThuoc: [
+        { tenKichThuoc: 'Nhỏ', giaThem: 0 },
+        { tenKichThuoc: 'Vừa', giaThem: 1.5 }
+      ]
+    });
 
     // Tạo admin
     await NguoiDung.create({
